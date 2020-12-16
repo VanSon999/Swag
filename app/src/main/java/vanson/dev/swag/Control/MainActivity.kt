d19461aa1e6080679f637ae619d717863e01f6b8
VanSon999 <nguyenvanson95999@gmail.com>
2020-12-16 16:54:44 +0700

    use adapter and create category layout item

--- app/src/main/java/vanson/dev/swag/Control/MainActivity.kt
+++ app/src/main/java/vanson/dev/swag/Control/MainActivity.kt
@@ -2,11 +2,20 @@ package vanson.dev.swag.Control
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import vanson.dev.swag.Model.Category
 import vanson.dev.swag.R
+import vanson.dev.swag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
