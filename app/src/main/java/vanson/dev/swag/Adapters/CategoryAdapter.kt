0e1c6cb5a1a8b907f94e4c5b796e882db0ae8251
VanSon999 <nguyenvanson95999@gmail.com>
2020-12-16 18:04:25 +0700

    customize adapter to build ui list view item

---
+++ app/src/main/java/vanson/dev/swag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,43 @@
+package vanson.dev.swag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import vanson.dev.swag.Model.Category
+import vanson.dev.swag.R
+//import java.text.FieldPosition
+
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+    val context = context
+    val categories = categories
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView: View = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
+        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+        println("Heavy Search")
+
+        val category = categories[position]
+
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        categoryImage.setImageResource(resourceId)
+        categoryName.text = category.title
+        return categoryView
+    }
+
+    override fun getItem(p0: Int): Any {
+        return categories[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+}
--- app/src/main/java/vanson/dev/swag/Control/MainActivity.kt
+++ app/src/main/java/vanson/dev/swag/Control/MainActivity.kt
@@ -4,18 +4,21 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import vanson.dev.swag.Adapters.CategoryAdapter
 import vanson.dev.swag.Model.Category
 import vanson.dev.swag.R
 import vanson.dev.swag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+//    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+//        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this,DataService.categories)
         categoryListView.adapter = adapter
     }
 }
